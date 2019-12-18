2aafaa3381c8d2ff837c0276211c28395f41ca16
liatmoss <liatmoss@hotmail.com>
2019-12-18 16:52:43 +0000

    added player 1 move

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,13 @@
 class Game
 
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  def player_x(pos)
+    i = pos
+    @board[i, 1] = "X"
+    @board
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,9 @@ require 'game'
 
 describe Game do
 
-  
+  it 'shows an x in one square if player x chooses 0' do
+    game = Game.new
+    expect(game.player_x(0)).to eq(["X", " ", " ", " ", " ", " ", " ", " ", " "])
+  end
 
 end
